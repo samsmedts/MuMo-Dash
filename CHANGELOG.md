23813b516d69b0ae78ddea4c95d8068883109322
samsmedts <samsmedts@gmail.com>
2020-09-05 11:35:04 +0200

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# CHANGELOG
+
+## V 3.0.0
+
+
+## V 2.0.0
+
+
+## V 1.0.0
+- Mockup front page of the Mumo Dashboard
+- 
---
+++ assets/js/off-canvas.js
@@ -0,0 +1,10 @@
+// Example of bootstrapDash ---------------------------------------
+
+(function($) {
+  'use strict';
+  $(function() {
+    $('[data-toggle="offcanvas"]').on("click", function() {
+      $('.sidebar-offcanvas').toggleClass('active')
+    });
+  });
+})(jQuery);
